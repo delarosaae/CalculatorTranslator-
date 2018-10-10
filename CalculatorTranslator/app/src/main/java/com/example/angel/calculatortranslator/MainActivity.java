89c81db6bb4bebf678197088a8d9de6a2c0cf62b
Angel de la Rosa <delarosaae@vcu.edu>
2018-10-10 00:00:19 -0400

    added the buttons to the first screen

--- CalculatorTranslator/app/src/main/java/com/example/angel/calculatortranslator/MainActivity.java
+++ CalculatorTranslator/app/src/main/java/com/example/angel/calculatortranslator/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.angel.calculatortranslator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    private String number = "";
+    private StringBuilder number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,7 @@ public class MainActivity extends Activity {
 
     public String getNumber()
     {
-        return number;
+        return number.toString();
     }
 
     /**
@@ -27,4 +29,54 @@ public class MainActivity extends Activity {
      *For every onclick, we will send that number to a method that will keep over string. String
      * might need to be global
      */
+
+    public void One(View view)
+    {
+        number.append("1");
+    }
+
+    public void Two(View view)
+    {
+        number.append("2");
+    }
+
+    public void Three(View view)
+    {
+        number.append("3");
+    }
+
+    public void Four(View view)
+    {
+        number.append("4");
+    }
+
+    public void Five(View view)
+    {
+        number.append("5");
+    }
+
+    public void Six(View view)
+    {
+        number.append("6");
+    }
+
+    public void Seven(View view)
+    {
+        number.append("7");
+    }
+
+    public void Eight(View view)
+    {
+        number.append("8");
+    }
+
+    public void Nine(View view)
+    {
+        number.append("9");
+    }
+
+    public void Zero(View view)
+    {
+        number.append("0");
+    }
 }
